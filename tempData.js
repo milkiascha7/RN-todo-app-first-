f5bd490c50e157957382c08f4389ceb1e867057e
Milkias Bekele <mikehailu504@gmail.com>
2022-04-04 15:08:36 +0300

    first commit

---
+++ tempData.js
@@ -0,0 +1,73 @@
+// temporary data until we connect with firebase
+
+export default tempData = [
+    {   
+        id:1,
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: "Book Flight",
+                completed: false
+            },
+            {
+                title: "Passport Check",
+                completed: true
+            },
+            {
+                title: "Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: "Pack Luggage",
+                completed: false
+            }
+        ]
+    },
+    {
+        id: 2,
+        name:"Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title:"Buy Milk",
+                completed: false
+            },
+            {
+                title: "Plan weekend camping trip",
+                completed: true,
+            },
+            {
+                title: "run 2 miles",
+                completed: true
+            },
+            {
+                title: "Make a video",
+                completed: true,
+            },
+            {
+                title: "Take dog for a walk",
+                completed: true,
+            }
+        ]
+    },
+    {
+        id: 3,
+        name: "Birthday Party",
+        color: "#595DD9",
+        todos: [
+            {
+                title:"Get Ballons",
+                completed: false,
+            },
+            {
+                title:"Send Invitations",
+                completed: true,
+            },
+            {
+                title:"Make Dinner Reservation",
+                completed: true,
+            }
+        ]
+    }
+]
